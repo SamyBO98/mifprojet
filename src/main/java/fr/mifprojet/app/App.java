9a968e97942b5a68ce204c252a3cab558f96b739
Azziz Otman <otman.azziz@etu.univ-lyon1.fr>
2020-09-29 16:24:08 +0200

    Projet maven généré (fixes #6)

---
+++ src/main/java/fr/mifprojet/app/App.java
@@ -0,0 +1,22 @@
+package fr.mifprojet.app;
+
+/**
+ * Classe principale App.
+ */
+public final class App {
+
+    /**
+     * Private constructor App.
+     */
+    private App() {
+
+    }
+
+    /**
+     * Classe principale.
+     * @param args arguments.
+     */
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+};
