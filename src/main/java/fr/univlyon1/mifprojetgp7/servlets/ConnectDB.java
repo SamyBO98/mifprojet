c5394243749301b9e5fcf68b6b4eff1818347abf
p1707136 <samy@DESKTOP-UVD77BR.localdomain>
2020-11-21 16:44:43 +0100

    Ignore system.out error in sonarqube

--- src/main/java/fr/univlyon1/mifprojetgp7/servlets/ConnectDB.java
+++ src/main/java/fr/univlyon1/mifprojetgp7/servlets/ConnectDB.java
@@ -10,9 +10,12 @@ import javax.servlet.http.HttpServlet;
 @WebServlet(name = "ConnectDB", loadOnStartup = 1, urlPatterns = "/")
 public class ConnectDB extends HttpServlet {
 
+    @java.lang.SuppressWarnings("squid:S2226")
     EntityManager em;
 
     @Override
+    @java.lang.SuppressWarnings("squid:S106")
+    //Enlève l'erreur du system.out pour vérifier de notre côté si la BD s'est bien lancée
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         this.em = Persistence.createEntityManagerFactory("mifprojetgp7-database").createEntityManager();
