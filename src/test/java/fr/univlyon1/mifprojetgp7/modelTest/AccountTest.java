49620e71bf0475044a304152610381ff98138d1b
Priska LAFAYE <Priska@clerc-PC.GBM>
2020-11-22 21:19:33 +0100

    Test

---
+++ src/test/java/fr/univlyon1/mifprojetgp7/modelTest/AccountTest.java
@@ -0,0 +1,55 @@
+package fr.univlyon1.mifprojetgp7.model;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class AccountTest {
+
+    Account userT = new Account();
+
+    @Test
+    public void TestEmailUser() {
+        System.out.println(">> TEST: Email User <<");
+
+        userT.setEmailUser("test@test.fr");
+        assertEquals("test@test.fr", userT.getEmailUser());
+        userT.setEmailUser(null);
+    }
+
+    @Test
+    public void TestName() {
+        System.out.println(">> TEST: Name <<");
+
+        userT.setName("Name");
+        assertEquals("Name", userT.getName());
+        userT.setName(null);
+    }
+
+    @Test
+    public void TestFirstName() {
+        System.out.println(">> TEST: First Name <<");
+
+        userT.setFirstName("FirstName");
+        assertEquals("FirstName", userT.getFirstName());
+        userT.setFirstName(null);
+    }
+
+    @Test
+    public void TestPassword() {
+        System.out.println(">> TEST: Password <<");
+
+        userT.setPassword("password");
+        assertEquals("password", userT.getPassword());
+        userT.setPassword(null);
+    }
+
+    @Test
+    public void TestSalt() {
+        System.out.println(">> TEST: Salt <<");
+
+        userT.setSalt("salt");
+        assertEquals("salt", userT.getSalt());
+        userT.setSalt(null);
+    }
+}
---
+++ src/test/java/fr/univlyon1/mifprojetgp7/modelTest/CategoryTest.java
@@ -0,0 +1,19 @@
+package fr.univlyon1.mifprojetgp7.model;
+
+import fr.univlyon1.mifprojetgp7.model.Category;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category categoryT = new Category();
+
+    @Test
+    public void TestCategory() {
+        System.out.println(">> TEST: Category <<");
+
+        categoryT.setCategoryName("Sport");
+        assertEquals("Sport", categoryT.getCategoryName());
+        categoryT.setCategoryName(null);
+    }
+}
