0f5e48b8530064895b9045fabab9b32c3adb14a2
RIBEIRO ANDGEL p1914838 <andgel.ribeiro@etu.univ-lyon1.fr>
2020-11-22 23:12:55 +0100

    add parseUri tests

---
+++ src/test/java/fr/univlyon1/mifprojetgp7/utilstest/ParseURITest.java
@@ -0,0 +1,25 @@
+package fr.univlyon1.mifprojetgp7.utilstest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import fr.univlyon1.mifprojetgp7.utils.ParseURI;
+
+public class ParseURITest {
+    
+    @Test
+    public void parseUri() {
+	List<String> uri = ParseURI.parseUri("/blabla/events/salut/coucou", "events");
+	assertEquals("salut", uri.get(0));
+	assertEquals("coucou", uri.get(1));
+    }
+    
+    @Test
+    public void sourceUri() {
+	String uri = ParseURI.sourceURI("/blabla/events/salut/coucou");
+	assertEquals("blabla", uri);
+    }
+}
